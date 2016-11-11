4dec33901950de21c4959bee31fa2622ef5c043d
Kevin Wojniak <kainjow@users.noreply.github.com>
2016-11-10 23:41:07 -0800

    Replace raw allocation with vector

--- game/GLImage_CoreImage.cpp
+++ game/GLImage_CoreImage.cpp
@@ -1,26 +1,39 @@
 #include "GLImage.h"
 #include <ApplicationServices/ApplicationServices.h>
+#include <vector>
 
 void GL::Image::load(const unsigned char *buf, size_t bufSize)
 {
-    CFDataRef data = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, (const UInt8*)buf, (CFIndex)bufSize, kCFAllocatorNull);
+    CFDataRef data = CFDataCreateWithBytesNoCopy(
+        kCFAllocatorDefault,
+        reinterpret_cast<const UInt8*>(buf),
+        static_cast<CFIndex>(bufSize),
+        kCFAllocatorNull
+    );
     if (data != NULL) {
         CGImageSourceRef imageSource = CGImageSourceCreateWithData(data, NULL);
         if (imageSource != NULL) {
             CGImageRef img = CGImageSourceCreateImageAtIndex(imageSource, 0, NULL);
             if (img != NULL) {
-                width_ = (int)CGImageGetWidth(img);
-                height_ = (int)CGImageGetHeight(img);
+                width_ = static_cast<int>(CGImageGetWidth(img));
+                height_ = static_cast<int>(CGImageGetHeight(img));
                 CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
                 if (colorSpace != NULL) {
-                    char *texData = (char*)calloc(width_ * height_ * 4, sizeof(char));
-                    CGContextRef ctx = CGBitmapContextCreate(texData, width_, height_, 8, width_ * 4, colorSpace, kCGBitmapByteOrder32Host | kCGImageAlphaPremultipliedFirst);
+                    std::vector<char> texData(static_cast<std::vector<char>::size_type>(width_ * height_ * 4));
+                    CGContextRef ctx = CGBitmapContextCreate(
+                        &texData[0],
+                        static_cast<size_t>(width_),
+                        static_cast<size_t>(height_),
+                        8,
+                        static_cast<size_t>(width_ * 4),
+                        colorSpace,
+                        kCGBitmapByteOrder32Host | kCGImageAlphaPremultipliedFirst
+                    );
                     if (ctx != NULL) {
                         CGContextDrawImage(ctx, CGRectMake(0.0, 0.0, width_, height_), img);
                         CGContextRelease(ctx);
-                        loadTextureData_(texData);
+                        loadTextureData_(&texData[0]);
                     }
-                    free(texData);
                     CGColorSpaceRelease(colorSpace);
                 }
                 CGImageRelease(img);
