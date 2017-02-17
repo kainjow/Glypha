9fbad02d92f5528784a9d788a1124b8c7a7d8a41
Kevin Wojniak <kainjow@users.noreply.github.com>
2017-02-16 23:03:25 -0800

    Enable visual styles

--- win/main.cpp
+++ win/main.cpp
@@ -1,3 +1,5 @@
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #include <windows.h>
 #include <gl/gL.h>
 #include "../game/GLGame.h"
